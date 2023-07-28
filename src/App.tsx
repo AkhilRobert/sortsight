232bef001bf78283c6109a36f469eb8f032be001
Akhil Robert <akhilrobert2412@gmail.com>
2023-07-28 23:00:49 +0530

    moved the logic to component from custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,79 @@
 import { generateRandomArray } from "./utils";
 import { Visualizer } from "./components/visualizer";
-import { useSortingAnimator } from "./hooks/use-sorting-animator";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TbTriangle, TbRepeat, TbSettings } from "react-icons/tb";
 import { bubbleSort } from "./algorithms/bubble-sort";
 import { selectionSort } from "./algorithms/selection-sort";
 import { insertionSort } from "./algorithms/insertion-sort";
+import { SortingAnim } from "./utils/types";
+
+const algorithms: Record<
+  string,
+  (list: number[]) => Generator<SortingAnim, void, unknown>
+> = {
+  "Bubble Sort": bubbleSort,
+  "Selection Sort": selectionSort,
+  "Insertion Sort": insertionSort,
+};
+
+const algorithmName = Object.keys(algorithms);
 
 const App = () => {
   const [listLength, setListLength] = useState<number>(30);
+  const [algorithm, setAlgorithm] = useState<string>(algorithmName[0]);
   const [showSettings, setShowSettings] = useState<boolean>(false);
+  const [speed, setSpeed] = useState(100);
+  const [started, setStarted] = useState(false);
+  const [data, setData] = useState<SortingAnim>({
+    list: generateRandomArray(listLength),
+  });
+  const [generator, setGenerator] = useState<
+    Generator<SortingAnim, void, unknown>
+  >(bubbleSort(data.list));
 
-  const list = generateRandomArray(listLength);
+  const canUpdateData = useRef<boolean>(false);
+  const canUpdateAlgorithm = useRef<boolean>(false);
 
-  const { data, startAnimation, started, changeData } = useSortingAnimator(
-    { list },
-    bubbleSort,
-    10
-  );
+  useEffect(() => {
+    // Don't update array on initial render
+    if (canUpdateData.current) {
+      setData({ list: generateRandomArray(listLength) });
+    } else {
+      canUpdateData.current = true;
+    }
+  }, [listLength]);
 
-  const algorithms = {
-    "Bubble Sort": bubbleSort,
-    "Selection Sort": selectionSort,
-    "Insertion Sort": insertionSort,
-  };
+  useEffect(() => {
+    // Don't update algorithm on initial render
+    if (canUpdateAlgorithm.current) {
+      console.log("me");
+      const fn = algorithms[algorithm];
+      setGenerator(fn(data.list));
+    } else {
+      canUpdateAlgorithm.current = true;
+    }
+  }, [algorithm, data.list]);
+
+  // Sorting animator
+  useEffect(() => {
+    let id: number;
+    if (started) {
+      id = setInterval(() => {
+        console.log(id);
+        const sorted = generator.next();
+        if (!sorted.done) {
+          setData(sorted.value);
+        } else {
+          clearInterval(id);
+          setStarted(false);
+        }
+      }, speed);
+    }
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [generator, started, speed]);
 
   return (
     <main>
@@ -34,18 +84,18 @@ const App = () => {
         className="border-black border-b-2 relative"
       >
         <div className="flex justify-between items-center h-[50px] max-w-[95%] m-auto">
-          <h1 className="text-2xl">{Object.keys(algorithms)[0]}</h1>
+          <h1 className="text-2xl">{algorithm}</h1>
           <div className="flex gap-4 items-center">
             <TbTriangle
               size={22}
               color={!started ? "black" : "var(--grey)"}
-              onClick={!started ? startAnimation : undefined}
+              onClick={!started ? () => setStarted(true) : undefined}
               className="rotate-90 cursor-pointer"
             />
             <TbRepeat
               color={!started ? "black" : "var(--grey)"}
               onClick={() => {
-                changeData({ list: generateRandomArray(listLength) });
+                setData({ list: generateRandomArray(listLength) });
               }}
               size={22}
               className="cursor-pointer"
@@ -62,7 +112,12 @@ const App = () => {
           <div className="floating-settings">
             <div className="input">
               <label htmlFor="algorithm">algorithm</label>
-              <select name="algorithm">
+              <select
+                onChange={(e) => {
+                  setAlgorithm(e.currentTarget.value);
+                }}
+                name="algorithm"
+              >
                 {Object.keys(algorithms).map((v, i) => (
                   <option key={`${i}-${v}`}>{v}</option>
                 ))}
@@ -73,7 +128,9 @@ const App = () => {
               <label htmlFor="arraySize">Array Size</label>
               <input
                 value={listLength}
-                onChange={(e) => setListLength(e.target.valueAsNumber)}
+                onChange={(e) => {
+                  setListLength(e.currentTarget.valueAsNumber);
+                }}
                 name="arraySize"
                 type="range"
               />
@@ -81,7 +138,14 @@ const App = () => {
 
             <div className="input">
               <label htmlFor="timing">Sorting speed</label>
-              <input name="timing" type="range" />
+              <input
+                value={speed}
+                onChange={(e) => {
+                  setSpeed(e.currentTarget.valueAsNumber);
+                }}
+                name="timing"
+                type="range"
+              />
             </div>
           </div>
         )}
